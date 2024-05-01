e2b80bce9ad7739578d28383ee5e0a7ea34c9611
Luis Carlos Silva Junior <luisccomp@gmail.com>
2024-05-01 18:40:57 -0300

    feat: criado componentes para items e detalhes do pedido.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css';
+import Item from './components/Item';
+import OrderDetails from './components/OrderDetails';
 
 // Todo componente deve começar com letra maiuscula
 function App() {
@@ -87,6 +89,8 @@ function App() {
         }
     ];
 
+    const hasItemsInBag = items.filter(item => item.isInBag).length > 0;
+
     const shopName = 'Jersey Shop Made with React JS';
 
     // Eh importante que todos os elementos de um componente estejam dentro de um unico componente raiz.
@@ -96,159 +100,10 @@ function App() {
             <section className="items">
                 <h4>{shopName}</h4>
                 
-                <div className="product selected">
-                    <div className="photo">
-                        <img src="././img/real_madrid.webp" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Real Madrid</span>
-                        <span className="price">$ 119.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/milan.png" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Milan</span>
-                        <span className="price">$ 99.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/chelsea.webp" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Chelsea</span>
-                        <span className="price">$ 99.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/barcelona.png" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Barcelona</span>
-                        <span className="price">$ 109.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/benfica.png" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Benfica</span>
-                        <span className="price">$ 89.49</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/manchester.webp" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Manchester City</span>
-                        <span className="price">$ 129.79</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/bayern.webp" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Bayern</span>
-                        <span className="price">$ 119.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/psg.png" />
-                    </div>
-                    <div className="description">
-                        <span className="name">PSG</span>
-                        <span className="price">$ 94.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                
-                <div className="product">
-                    <div className="photo">
-                        <img src="./img/ajax.webp" />
-                    </div>
-                    <div className="description">
-                        <span className="name">Ajax</span>
-                        <span className="price">$ 89.99</span>
-                        <div className="quantity-area">
-                            <button>-</button>
-                            <span className="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            
+                {items.map(item => <Item item={item} key={item.id} />)}
+            </section>            
 
-            <section className="summary">
-                <strong>Order Details</strong>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Item</th>
-                            <th>Total</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1x Real Madrid</td>
-                            <td>$ 119.99</td>
-                        </tr>
-                        
-                        <tr>
-                            <th>Total</th>
-                            <th>$ 119.99</th>
-                        </tr>
-                    </tbody>
-                </table>
-            </section>
-            
+            {hasItemsInBag && <OrderDetails/>}
         </>
     );
 }
