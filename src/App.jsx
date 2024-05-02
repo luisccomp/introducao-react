479e455c40449bbcc7fc8dcdcd6b59e203633b6b
Luis Carlos Silva Junior <luisccomp@gmail.com>
2024-05-02 18:59:09 -0300

    Adicionado o evento onClick no componente Item

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,123 @@
+import { useState } from 'react';
 import './App.css';
 import Item from './components/Item';
 import OrderDetails from './components/OrderDetails';
 
 // Todo componente deve começar com letra maiuscula
 function App() {
+    const shopName = 'Jersey Shop Made with React JS';
 
-    const items = [
-        {
-            
-            id: 1, 
-            photo: "real_madrid.webp",
-            name: "Real Madrid",
-            price: 119.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 2, 
-            photo: "milan.png",
-            name: "Milan",
-            price: 99.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 3, 
-            photo: "chelsea.webp",
-            name: "Chelsea",
-            price: 99.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 4, 
-            photo: "barcelona.png",
-            name: "Barcelona",
-            price: 109.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 5, photo: "benfica.png",
-            name: "Benfica",
-            price: 89.49,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 6, 
-            photo: "manchester.webp",
-            name: "Manchester City",
-            price: 129.79,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 7, 
-            photo: "bayern.webp",
-            name: "Bayern",
-            price: 119.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 8, 
-            photo: "psg.png",
-            name: "PSG",
-            price: 94.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 9, 
-            photo: "ajax.webp",
-            name: "Ajax",
-            price: 89.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        }
-    ];
+    const [items, setItems] = useState(
+        [
+            {
+                
+                id: 1, 
+                photo: "real_madrid.webp",
+                name: "Real Madrid",
+                price: 119.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 2, 
+                photo: "milan.png",
+                name: "Milan",
+                price: 99.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 3, 
+                photo: "chelsea.webp",
+                name: "Chelsea",
+                price: 99.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 4, 
+                photo: "barcelona.png",
+                name: "Barcelona",
+                price: 109.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 5, photo: "benfica.png",
+                name: "Benfica",
+                price: 89.49,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 6, 
+                photo: "manchester.webp",
+                name: "Manchester City",
+                price: 129.79,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 7, 
+                photo: "bayern.webp",
+                name: "Bayern",
+                price: 119.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 8, 
+                photo: "psg.png",
+                name: "PSG",
+                price: 94.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 9, 
+                photo: "ajax.webp",
+                name: "Ajax",
+                price: 89.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            }
+        ]
+    );
 
     const hasItemsInBag = items.filter(item => item.isInBag).length > 0;
 
-    const shopName = 'Jersey Shop Made with React JS';
+    const handleSelectItem = id => {
+        const indexFound = items.findIndex(item => item.id === id);
+
+        setItems(items.map((item, index) => index === indexFound ? { ...item, isInBag: !item.isInBag } : item));
+    };
 
     // Eh importante que todos os elementos de um componente estejam dentro de um unico componente raiz.
     // Pode ser dentro de um unico div ou dentro de um React Fragment <>...</>
+
+    // Sempre que o meu componente receber um ID como parâmetro, é importante que ele seja declarado como uma
+    // arrow function com todos os parametros
     return ( 
         <>
             <section className="items">
                 <h4>{shopName}</h4>
                 
-                {items.map(item => <Item item={item} key={item.id} />)}
+                {items.map(item => 
+                    <Item 
+                        item={item}
+                        key={item.id}
+                        onClick={id => handleSelectItem(id)}
+                    />
+                )}
             </section>            
 
             {hasItemsInBag && <OrderDetails/>}
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,7 +1,11 @@
-const Item = ({ item }) => {
+// Sempre que um componente react recebe parametros, ele vem dentro de um objeto desestruturado. O conceito disso se
+// chama props. No React as props sao uma forma de passar informação a um componente. Ex:
+// <Componente prop1='Hello' prop2='World' />
+// const Componente = ({prop1, prop2}) => {...};
+const Item = ({ item, onClick }) => {
     return (
         <>
-            <div className={`product ${item.isInBag ? 'selected' : ''}`.trim()}>
+            <div onClick={() => onClick(item.id)} className={`product ${item.isInBag ? 'selected' : ''}`.trim()}>
                 <div className="photo">
                     <img src={`./img/${item.photo}`} />
                 </div>
